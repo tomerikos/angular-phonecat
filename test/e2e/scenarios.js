e4d820a341e649a282512318ed3e50383d95e674
Tomer <tomerfloyd@gmail.com>
2013-12-25 22:38:21 +0200

    fixed e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,20 +1,46 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+/* e2e-testing */
 
-describe('my app', function() {
+describe('PhoneCat App', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('app/index.html');
-  });
+  describe('Phone list view', function() {
 
-});
-
-
-    it('should render phone specific links', function() {
-      input('query').enter('nexus');
-      element('.phones li a').click();
-      expect(browser().location().url()).toBe('/phones/nexus-s');
+    beforeEach(function() {
+      browser().navigateTo('app/index.html');
     });
 
 
+    it('should filter the phone list as user types into the search box', function() {
+      expect(repeater('.phones li').count()).toBe(20);
+
+      input('phoneQuery').enter('nexus');
+      expect(repeater('.phones li').count()).toBe(1);
+
+      input('phoneQuery').enter('motorola');
+      expect(repeater('.phones li').count()).toBe(8);
+    });
+
+
+    it('should be possible to control phone order via the drop down select box', function() {
+      input('phoneQuery').enter('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                   "MOTOROLA XOOM\u2122"]);
+
+      select('phonesOrder').option('Alphabetical');
+
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["MOTOROLA XOOM\u2122",
+                   "Motorola XOOM\u2122 with Wi-Fi"]);
+    });
+
+
+    it('should render phone specific links', function() {
+      input('phoneQuery').enter('nexus');
+      element('.phones li a').click();
+      expect(browser().location().url()).toBe('/phones/nexus-s');
+    });
+  });
+});
