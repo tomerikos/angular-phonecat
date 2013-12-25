52dfa2f7a549c95b0a930061406dcf926b6cb279
Tomer <tomerfloyd@gmail.com>
2013-12-25 21:07:56 +0200

    some fixes

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,3 +9,12 @@ describe('my app', function() {
   });
 
 });
+
+
+    it('should render phone specific links', function() {
+      input('query').enter('nexus');
+      element('.phones li a').click();
+      expect(browser().location().url()).toBe('/phones/nexus-s');
+    });
+
+
